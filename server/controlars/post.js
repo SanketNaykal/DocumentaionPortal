6bbdd7922644189d5e7f9d38d7c3cc3c29cd8d03
SanketNaykal <naykalsanket@gmail.com>
2025-07-31 21:23:01 +0530

    tokens

--- server/controlars/post.js
+++ server/controlars/post.js
@@ -54,6 +54,8 @@ export const addPost = async (req, res) => {
 export const updatePost = async (req, res) => {
     try {
         const token = req.cookies.access_token;
+        console.log(token);
+        console.log(req.cookies.access_token);
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized!' });
         }
